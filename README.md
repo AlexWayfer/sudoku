1d66ca56012662d60396ee8187050d2d3830240e
Alexander Popov <alex.wayfer@gmail.com>
2023-09-27 11:59:17 +0300

    Add deployment script, command and instructions

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ Run `pnpm dev` for simple HTTP development server.
 
 We need for such thing because of CORS for native browser `import`/`export` in JavaScript.
 
+## Deployment
+
+Duplicate `deploy.example.yaml` file in `config` directory as `deploy.yaml`.
+Ensure to fill it correctly.
+
+Run `pnpm run deploy`.
+
 ## Credentials
 
 Button icons: https://www.svgrepo.com/collection/solar-linear-icons/
---
+++ exe/deploy.js
@@ -0,0 +1,38 @@
+const
+	fs = require('fs'),
+	path = require('path'),
+	YAML = require('yaml'),
+	{ exec } = require('child_process')
+
+const
+	rootDir = path.resolve(__dirname, '..'),
+	configFile = fs.readFileSync(path.resolve(rootDir, 'config/deploy.yaml'), 'utf8'),
+	config = YAML.parse(configFile)
+
+config.forEach(server => {
+	// console.debug(server.ssh)
+	// console.debug(server.path)
+
+	const
+		files = [
+			path.resolve(rootDir, 'images'),
+			path.resolve(rootDir, 'scripts'),
+			path.resolve(rootDir, 'styles'),
+			path.resolve(rootDir, 'index.html')
+		],
+		command = `scp -r ${files.join(' ')} ${server.ssh}:${server.path}`
+
+	exec(command, (error, stdout, stderr) => {
+		if (error) {
+			console.error(error)
+			return
+		}
+
+		if (stderr) {
+			console.error(stderr)
+			return
+		}
+
+		console.log(stdout)
+	})
+})
